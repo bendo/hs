fbcec5b86f2cd6c0131c7d81b7f2ed96eb1d2deb
bendo <zdeno.osina@gmail.com>
2017-09-05 22:59:59 +0200

    Finish first homework

--- cis194/hw1.hs
+++ cis194/hw1.hs
@@ -1,11 +1,20 @@
-module one
-where
+module One where
 
 toDigits :: Integer -> [Integer]
+toDigits 0 = []
+toDigits x = toDigits(x `div` 10) ++ [x `mod` 10]
+
 toDigitsRev :: Integer -> [Integer]
+toDigitsRev 0 = []
+toDigitsRev x = x `mod` 10 : toDigitsRev(x `div` 10)
 
 doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther [x] = [x]
+doubleEveryOther (x:y:xs) = x : y * 2 : doubleEveryOther xs
 
 sumDigits :: [Integer] -> Integer
+sumDigits xs = sum $ concat $ map toDigits xs
 
 validate :: Integer -> Bool
+validate x = mod (sumDigits $ doubleEveryOther $ toDigitsRev x) 10 == 0
