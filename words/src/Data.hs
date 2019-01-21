756dda0ea8dbf677760197d5a51c0e0b05f36e4c
bendo <zdeno.osina@gmail.com>
2019-01-21 17:27:12 +0100

    Add words game

---
+++ words/src/Data.hs
@@ -0,0 +1,30 @@
+module Data
+    ( grid
+    , languages
+    ) where
+
+grid = [ "____________"
+       , "___HASKELL__"
+       , "___S___L____"
+       , "___A__LMTH__"
+       , "___B________"
+       , "_JAVASCRIPT_"
+       , "__A___S_L___"
+       , "___V__S__R__"
+       , "____AQ____E_"
+       , "____L_NOHTYP"
+       ]
+
+languages = [ "HASKELL"
+            , "ELM"
+            , "PERL"
+            , "CSS"
+            , "JAVA"
+            , "JAVASCRIPT"
+            , "HTML"
+            , "SQL"
+            , "PYTHON"
+            , "BASH"
+            ]
+
+
---
+++ words/test/Spec.hs
@@ -0,0 +1,22 @@
+import Test.Hspec
+import Data
+import Lib
+
+main :: IO ()
+main = hspec $ do
+    describe "formatGrid" $ do
+        it "Should concatenate every line with a new line" $ do
+            formatGrid ["abc", "def", "ghi"] `shouldBe` "abc\ndef\nghi\n"
+
+    describe "findWord" $ do
+        it "Should find words that exists on the Grid" $ do
+            findWord grid "HASKELL" `shouldBe` Just "HASKELL"
+            findWord grid "PERL" `shouldBe` Just "PERL"
+        it "Should not find words that does not exist on the Grid" $ do
+            findWord grid "COBOL" `shouldBe` Nothing
+
+    describe "findWords" $ do
+        it "Should find all the words that exist on the Grid" $ do
+            findWords grid languages `shouldBe` languages
+        it "Should not find words that do not exist on the Grid" $ do
+            findWords grid ["REACT", "COBOL"] `shouldBe` []
